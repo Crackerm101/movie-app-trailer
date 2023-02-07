42b120981e215bdc1e853fdc6cd98c5eeadd82c4
Jayvee-Friday <91900181+Crackerm101@users.noreply.github.com>
2023-02-08 03:07:19 +0800

    Update MovieCard.jsx

--- src/Components/Allmovies/MovieCard.jsx
+++ src/Components/Allmovies/MovieCard.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-
 // Import Css
 import "./UniversalMovie.css";
 
