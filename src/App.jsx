fb56d60256d6b70f0d6508edb8f63c14d9f52399
Jayvee-Friday <91900181+Crackerm101@users.noreply.github.com>
2023-02-08 03:07:14 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,33 +12,39 @@ const Contact = lazy(() => import("./Pages/Contact"));
 
 const Movie = lazy(() => import("./Pages/Movie"));
 
-const MovieRow = lazy(() => import("./Pages/MovieRow"));
+const MovieRow = lazy(() => import("./Components/Allmovies/MovieRow"));
+const TVshows = lazy(() => import("./Pages/TVshows"));
 
 function App() {
-  return (
-    <Router>
-      <Suspense
-        fallback={
-          <div className="Loader_con">
-            <span className="loader"></span>
-          </div>
-        }
-      >
-        <Navbar />
-        <Routes>
-          <Route path="/">
-            <Route index element={<HeaderHomepage />} />
-            <Route index element={<MovieRow />} />
-            <Route exact path="CategoryPage" element={<CategoryPage />} />
-            <Route exact path="movie/:id" element={<Movie />} />
-            <Route exact path="Price" element={<Price />} />
-            <Route exact path="Contact" element={<Contact />} />
-          </Route>
-        </Routes>
-        <Footer />
-      </Suspense>
-    </Router>
-  );
+    return (
+        <Router>
+            <Suspense
+                fallback={
+                    <div className="Loader_con">
+                        <span className="loader"></span>
+                    </div>
+                }
+            >
+                <Navbar />
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<HeaderHomepage />} />
+                        <Route index element={<MovieRow />} />
+                        <Route
+                            exact
+                            path="CategoryPage"
+                            element={<CategoryPage />}
+                        />
+                        <Route exact path="movie/:id" element={<Movie />} />
+                        <Route exact path="TVshows" element={<TVshows />} />
+                        <Route exact path="Price" element={<Price />} />
+                        <Route exact path="Contact" element={<Contact />} />
+                    </Route>
+                </Routes>
+                <Footer />
+            </Suspense>
+        </Router>
+    );
 }
 
 export default App;
